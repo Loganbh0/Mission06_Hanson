ed365abf5e017e0f67629717bd03ea69e43e5317
Loganbh0 <loganbrianhanson@gmail.com>
2026-02-11 14:59:59 -0700

    Add project files.

---
+++ Mission06_Hanson/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission06_Hanson.Models;
+
+namespace Mission06_Hanson.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly MovieDbContext _context;
+
+        public MovieController(MovieDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult EnterMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovie(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(movie);
+                _context.SaveChanges();
+
+                return RedirectToAction("Confirmation");
+            }
+
+            return View(movie);
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Mission06_Hanson/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Hanson.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string Director { get; set; } = string.Empty;
+
+        [Required]
+        public string Rating { get; set; } = string.Empty; // G, PG, PG-13, R
+
+        public bool Edited { get; set; } // true/false (yes/no)
+
+        public string? LentTo { get; set; }
+
+        [StringLength(25)]
+        public string? Notes { get; set; }
+    }
+}
